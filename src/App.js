0c9a6d7a7d3da45a12c787f88235303250571773
Mateusz <ryniuss@o2.pl>
2023-01-19 18:20:17 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import styled from 'styled-components';
-
-const StyledFirstText = styled.h2`
-	font-size: 20px;
-	color: green;
-`;
-
-const StyledSecondText = styled.h3`
-	font-size: 15px;
-	color: red;
-`;
+import styled, { isStyledComponent, keyframes } from 'styled-components';
+import logo from './logo.svg';
 
 const App = () => {
 	return (
-		<div>
-			<StyledFirstText>Text1</StyledFirstText>
-			<StyledSecondText>Text2</StyledSecondText>
-			<h4>Text34</h4>
-		</div>
+		<>
+			<AppWraper>
+				<p>center</p>
+				<AppHeader>
+					<AppLogo src={logo} alt='logo'></AppLogo>
+					<AppLink>
+						<p>test2</p>
+					</AppLink>
+					<Button>default</Button>
+					<TomatoButton>tomato</TomatoButton>
+				</AppHeader>
+			</AppWraper>
+		</>
 	);
 };
 
+const AppLink = styled.p`
+	color: #61dafb;
+`;
+
+const Button = styled.button`
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid palegoldenrod;
+	border-radius: 3px;
+	color: palevioletred;
+	font-size: 1em;
+`;
+
+const TomatoButton = styled.button`
+	border-color: tomato;
+	color: tomato;
+`;
+
+const appLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const AppWraper = styled.div`
+	text-align: center;
+`;
+
+const AppHeader = styled.header`
+	background-color: ${({ color }) => (color ? '#ff00000' : '#fff')};
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-size: calc(10px + 2vmin);
+	color: white;
+	z-index: -1;
+`;
+
+const AppLogo = styled.img`
+	height: 40vmin;
+	pointer-events: none;
+	z-index: 1000;
+
+	@media (prefers-reduced-motion: no-preference) {
+		animation: ${appLogoSpin} infinite 20s linear;
+	}
+`;
+
 export default App;
+
+// const StyledFirstText = styled.h2`
+// 	font-size: 20px;
+// 	color: green;
+// `;
+
+// const StyledSecondText = styled.h3`
+// 	font-size: 15px;
+// 	color: red;
+// `;
+
+/* <StyledFirstText>Text1</StyledFirstText>
+			<StyledSecondText>Text2</StyledSecondText>
+			<h4>Text3</h4> */
